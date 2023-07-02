60c2a1d33ab29ddfbbad04f5928056bb8f441e5c
Marcos Vinícios <contato@marcoswolf.com.br>
2023-07-02 00:22:26 -0300

    Inserido fatoração;

--- calculadora-cs/Program.cs
+++ calculadora-cs/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -29,6 +30,22 @@ namespace calculadora_cs
             return (a * b);
         }
 
+        public static List<int> calcFactorization(int a)
+        {
+            var primes = new List<int>();
+
+            for (int div = 2; div <= a; div++)
+            {
+                while (a % div == 0)
+                {
+                    primes.Add(div);
+                    a = a / div;
+                }
+            }
+
+            return primes;
+        }
+
         public static void interfaceCalculation(char menuChoice)
         {
             int auxChoice = (int)Char.GetNumericValue(menuChoice);
@@ -45,20 +62,31 @@ namespace calculadora_cs
                 if (auxChoice == 1)
                 {
                     Console.WriteLine("\nO resultado é: " + calcSum(auxA, auxB));
-                    Console.ReadKey();
                 } else if (auxChoice == 2)
                 {
                     Console.WriteLine("\nO resultado é: " + calcSubtract(auxA, auxB));
-                    Console.ReadKey(true);
                 } else if (auxChoice == 3)
                 {
                     Console.WriteLine("\nO resultado é: " + calcDivide(auxA, auxB));
-                    Console.ReadKey(true);
                 } else if (auxChoice == 4)
                 {
                     Console.WriteLine("\nO resultado é: " + calcMultiply(auxA, auxB));
-                    Console.ReadKey(true);
                 }
+                
+                Console.Write("\nPressione qualquer tecla para continuar");
+                Console.ReadKey();
+            } else if (auxChoice == 5)
+            {
+                Console.Write("Insira o valor: ");
+                int auxA = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("\nO resultado é: ");
+                foreach (int fatora in calcFactorization(auxA)) {
+                    Console.WriteLine(fatora);
+                }
+
+                Console.Write("\nPressione qualquer tecla para continuar");
+                Console.ReadKey();
             }
         }
 
@@ -68,6 +96,7 @@ namespace calculadora_cs
             Console.Write("Calculadora de Console em C# \n---------------------------------\n");
             Console.WriteLine("1 - Soma    2 - Subtração");
             Console.WriteLine("3 - Divisão 4 - Multiplicação");
+            Console.WriteLine("5 - Fatoração");
             Console.Write("\nSelecione uma opção: ");
             
             interfaceCalculation(Console.ReadKey().KeyChar);
