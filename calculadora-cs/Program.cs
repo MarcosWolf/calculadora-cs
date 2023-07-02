7e1660be344cbf919ec0cf06018268318562bcf8
Marcos Vinícios <contato@marcoswolf.com.br>
2023-07-01 23:26:35 -0300

    Inserido divisão e multiplicação; estrutura de obter dados A e B reorganizados para diminuir o código

--- calculadora-cs/Program.cs
+++ calculadora-cs/Program.cs
@@ -19,30 +19,46 @@ namespace calculadora_cs
             return (a - b);
         }
 
-        public static void interfaceCalculo(char menuChoice)
+        public static double calcDivide(double a, double b)
+        {
+            return (a / b);
+        }
+
+        public static double calcMultiply(double a, double b)
+        {
+            return (a * b);
+        }
+
+        public static void interfaceCalculation(char menuChoice)
         {
             int auxChoice = (int)Char.GetNumericValue(menuChoice);
             Console.Clear();
             Console.Write("Calculadora de Console em C# \n---------------------------------\n");
 
-            if (auxChoice == 1)
+            if (auxChoice >= 1 && auxChoice <= 4)
             {
                 Console.Write("Insira o primeiro valor: ");
                 double auxA = Convert.ToDouble(Console.ReadLine());
                 Console.Write("Insira o segundo valor: ");
                 double auxB = Convert.ToDouble(Console.ReadLine());
 
-                Console.WriteLine("\nO resultado é: " + calcSum(auxA, auxB));
-                Console.ReadKey();
-            } else if (auxChoice == 2)
-            {
-                Console.Write("Insira o primeiro valor: ");
-                double auxA = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Insira o segundo valor: ");
-                double auxB = Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("\nO resultado é: " + calcSubtract(auxA, auxB));
-                Console.ReadKey(true);
+                if (auxChoice == 1)
+                {
+                    Console.WriteLine("\nO resultado é: " + calcSum(auxA, auxB));
+                    Console.ReadKey();
+                } else if (auxChoice == 2)
+                {
+                    Console.WriteLine("\nO resultado é: " + calcSubtract(auxA, auxB));
+                    Console.ReadKey(true);
+                } else if (auxChoice == 3)
+                {
+                    Console.WriteLine("\nO resultado é: " + calcDivide(auxA, auxB));
+                    Console.ReadKey(true);
+                } else if (auxChoice == 4)
+                {
+                    Console.WriteLine("\nO resultado é: " + calcMultiply(auxA, auxB));
+                    Console.ReadKey(true);
+                }
             }
         }
 
@@ -54,7 +70,7 @@ namespace calculadora_cs
             Console.WriteLine("3 - Divisão 4 - Multiplicação");
             Console.Write("\nSelecione uma opção: ");
             
-            interfaceCalculo(Console.ReadKey().KeyChar);
+            interfaceCalculation(Console.ReadKey().KeyChar);
 
         }
         
